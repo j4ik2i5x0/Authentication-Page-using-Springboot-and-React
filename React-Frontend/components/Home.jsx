ae96244e4b05de0d249cc347ffa318a2dd753752
j4ik2i5x0 <jaikris24@gmail.com>
2025-06-02 18:41:02 +0530

    Moved public and src folders and files into React-frontend folder

---
+++ React-Frontend/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome Hom!</h1>
+      <p>Your budget tracker app is ready.</p>
+    </div>
+  );
+}
+
+export default Home;
